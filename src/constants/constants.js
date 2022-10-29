0e5c614244959a5a342a8c812dfd1f20403f44dd
Vivek-Vardhan-K <87512504+Vivek-Vardhan-K@users.noreply.github.com>
2022-10-29 13:48:33 +0530

    redirect to heroku https

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const cst={
-    base_url:'http://zorro-env.eba-zvyc3njq.ap-south-1.elasticbeanstalk.com/'
+    base_url:'https://cors-everywhere.herokuapp.com/http://zorro-env.eba-zvyc3njq.ap-south-1.elasticbeanstalk.com/'
 }
 
 export default cst;
